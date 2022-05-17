4d1df52f652dd80a70a1633d948709bd48f3d0c2
Everton Martins <3532everton@gmail.com>
2022-05-17 15:52:49 -0300

    alterações de código

---
+++ js/autenticar.js
@@ -0,0 +1,17 @@
+// Variáveis
+
+const usuario = document.getElementById('usuario');
+const labelUsuario = document.getElementById('labelUsuario');
+let validUsuario = false
+
+const senha = document.getElementById('senha');
+const labelSenha = document.getElementById('labelSenha');
+let validSenha = false
+
+const msgError = document.getElementById('msgError')
+
+
+// Entrada do usuário
+function entrar(){
+    alert('certo')
+}
