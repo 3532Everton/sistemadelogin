66a9492ac7d1ae1fa7093623ee977fd33bceb864
Everton Martins <3532everton@gmail.com>
2022-05-17 17:07:39 -0300

    finalização

--- js/autenticar.js
+++ js/autenticar.js
@@ -1,17 +1,50 @@
-// Variáveis
-
-const usuario = document.getElementById('usuario');
-const labelUsuario = document.getElementById('labelUsuario');
-let validUsuario = false
-
-const senha = document.getElementById('senha');
-const labelSenha = document.getElementById('labelSenha');
-let validSenha = false
-
-const msgError = document.getElementById('msgError')
 
 
 // Entrada do usuário
 function entrar(){
-    alert('certo')
+    // Variáveis
+    const usuario = document.getElementById('usuario');
+    const labelUsuario = document.getElementById('labelUsuario');
+    
+    const senha = document.getElementById('senha');
+    const labelSenha = document.getElementById('labelSenha');
+    
+    const msgError = document.getElementById('msgError')
+    
+    let listaUser = []
+    let userValid = {
+        nome: '',
+        user: '',
+        senha: ''
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'))
+
+    listaUser.forEach((item) => {
+        if(usuario.value == item.userCad && senha.value == item.senhaCad){
+            userValid = {
+                nome: item.nomeCad,
+                user: item.userCad,
+                senha: item.senhaCad
+            }
+        }
+    });
+
+    if (usuario.value == userValid.user && senha.value == userValid.senha) {
+        window.location.href = ''
+        
+        let token = Math.random().toString(16).substr(2)
+        localStorage.setItem('token', token)
+
+        localStorage.setItem('userLogado', JSON.stringify(userValid))
+    } else {
+        labelUsuario.setAttribute('style', 'color: red; border-color: red;')
+        usuario.setAttribute('style', 'border-color: red;')
+        labelSenha.setAttribute('style', 'color: red; border-color: red;')
+        senha.setAttribute('style', 'border-color: red;')
+        msgError.setAttribute('style', 'display: block;')
+        msgError.innerText = 'Usuário ou senha incorretos'
+        usuario.focus()
+
+    }
 }
